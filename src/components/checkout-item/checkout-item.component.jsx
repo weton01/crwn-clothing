7592b12008d57545e5557ab7f53e94157115a34b
weton01 <43674958+weton01@users.noreply.github.com>
2019-08-26 16:15:11 -0300

    added addItem and removeItem functionality ot checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: {name, imageUrl, price, quantity} }) => (
+import {connect} from 'react-redux';
+
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
+
+
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem  }) => {
+ 
+ const { name, imageUrl, price, quantity } = cartItem;
+
+ return(
  <div className="checkout-item">
   <div className="image-container">
    <img src={imageUrl} alt="item"/>
@@ -11,16 +20,28 @@ const CheckoutItem = ({ cartItem: {name, imageUrl, price, quantity} }) => (
    {name}
   </span>
   <span className="quantity">
-   {quantity}
+   <div className='arrow' onClick={() => removeItem(cartItem)}> &#10094; </div>
+   <span className="value"> {quantity}</span>
+    <div className='arrow' onClick={() => addItem(cartItem)}> &#10095; </div>
   </span>
   <span className="price">
    {price}
   </span>
-  <span className="remove-button">
+   <span className="remove-button" onClick={() => clearItem(cartItem)}>
    &#10005;
   </span>
  </div>
 
-)
+)}
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+ clearItem: item => dispatch(clearItemFromCart(item)),
+ addItem: item => dispatch(addItem(item)),
+ removeItem: item => dispatch(removeItem(item))
+ 
+})
+
+export default connect(
+ null,
+ mapDispatchToProps
+ )(CheckoutItem);
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
