461b81c38717cf4348b13bea47b44e2921cc9357
weton01 <43674958+weton01@users.noreply.github.com>
2019-08-26 12:40:31 -0300

    added checkout page, checkout item component and updated cart to use selectors from reasoning

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,13 +2,15 @@ import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
-const selectUser = state => state.user;
-
 export const selectCartItems = createSelector(
  [selectCart],
- cart => cart.cartItems
+  cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+ [selectCart],
+ cart => cart.hidden
+)
 
 export const selectCartItemsCount = createSelector(
  [selectCartItems],
@@ -16,6 +18,16 @@ export const selectCartItemsCount = createSelector(
  cartItems.reduce(
   (accumalatedQuantity, cartItem) =>
   accumalatedQuantity + cartItem.quantity,
+  0
+ )
+)
+
+export const selectCartTotal = createSelector(
+ [selectCartItems],
+ cartItems => 
+ cartItems.reduce(
+  (accumalatedQuantity, cartItem) =>
+  accumalatedQuantity + cartItem.quantity * cartItem.price,
   0
  )
 )
